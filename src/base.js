f2aa7dcf0b90993b8983ad45ea2fcaa09e335748
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-01 08:56:17 -0500

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,4 +1,10 @@
 // webAgentLib for JavaScript
 // (c) speedyplane2247 2017-2019
 var webAgentLib = new Object();
+var webAgentStrings = new Object();
+var webAgentSplice = new Object();
+var webAgentInfo = new Object();
+// DEFINE BASE VARS
 webAgentLib.ver = "v0.9"
+webAgentLib.currentAgent = navigator.userAgent
+// DEFINE INFO VARS (BLANK)
