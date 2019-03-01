553de32ca5ca6ee160618889dc270c9366529ed1
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-01 08:58:12 -0500

    Update base.js

--- src/base.js
+++ src/base.js
@@ -7,4 +7,7 @@ var webAgentInfo = new Object();
 // DEFINE BASE VARS
 webAgentLib.ver = "v0.9"
 webAgentLib.currentAgent = navigator.userAgent
-// DEFINE INFO VARS (BLANK)
+// DEFINE INFO VARS (BLANK)  
+// Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405
+webAgentInfo.Mozilla = 0
+webAgentInfo.MozillaVer = 0
